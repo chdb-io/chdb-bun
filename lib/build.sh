f6044a3f893bbabb1d220a3c996c23da6c0fdf8b
auxten <auxtenwpc@gmail.com>
2023-12-29 18:13:07 +0800

    Fix session class

--- lib/build.sh
+++ lib/build.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 if [ "$(uname)" == "Darwin" ]; then
-    gcc -dynamiclib -o libchdb_bun.dylib -L. -lchdb libchdb_bun.c 
+    clang -O3 -dynamiclib -o chdb_bun.so -L. -lchdb chdb_bun.c 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    gcc -shared -fPIC -o libchdb_bun.so libchdb_bun.c -L. -lchdb
+    clang -O3 -shared -fPIC -o chdb_bun.so -L. -lchdb chdb_bun.c
 else
     echo "Unsupported operating system"
     exit 1
 fi
+
+mv chdb_bun.so ../chdb_bun.so
+mv libchdb.so ../libchdb.so
